84f7fa8c75ddb8d8205e99385fc4b919f9e54c40
sayross1995 <mohamedsaiss95@gmail.com>
2023-08-21 07:31:25 -0700

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,3 +1,5 @@
+#include "shell.h"
+
 /**
  * interactive - returns true if shell is in interactive mode
  * @info: struct address
@@ -6,11 +8,7 @@
  */
 int interactive(info_t *info)
 {
-<<<<<<< HEAD
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-=======
-        return (isatty(STDIN_FILENO) && info->readfd <= 2);
->>>>>>> 0dc74d5db6e88cc9cc50667ddaa49744cb861f25
 }
 
 /**
@@ -21,7 +19,6 @@ int interactive(info_t *info)
  */
 int is_delim(char c, char *delim)
 {
-<<<<<<< HEAD
 	while (*delim)
 	{
 		if (*delim == c)
@@ -29,15 +26,6 @@ int is_delim(char c, char *delim)
 		delim++;
 	}
 	return (0);
-=======
-        while (*delim)
-        {
-                if (*delim == c)
-                        return (1);
-                delim++;
-        }
-return (0);
->>>>>>> 0dc74d5db6e88cc9cc50667ddaa49744cb861f25
 }
 
 /**
@@ -47,19 +35,11 @@ return (0);
  */
 int custom_isalpha(int c)
 {
-<<<<<<< HEAD
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	return (1);
 	else
 		return (0);
-=======
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-                return (1);
- return (1);
-        else
-                return (0);
->>>>>>> 0dc74d5db6e88cc9cc50667ddaa49744cb861f25
 }
 
 /**
@@ -69,7 +49,6 @@ int custom_isalpha(int c)
  */
 int custom_atoi(char *s)
 {
-<<<<<<< HEAD
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 	for (i = 0; s[i] != '\0' && flag != 2; i++)
@@ -92,31 +71,5 @@ int custom_atoi(char *s)
 	else
 		output = result;
 	return (output);
-=======
-        int i, sign = 1, flag = 0, output;
-        unsigned int result = 0;
-        for (i = 0; s[i] != '\0' && flag != 2; i++)
-        {
-                if (s[i] == '-')
-                        sign *= -1;
-
-                if (s[i] >= '0' && s[i] <= '9')
-                {
-                        flag = 1;
-                        result *= 10;
-                        result += (s[i] - '0');
-                }
-                else if (flag == 1)
-                {
-                        flag = 2;
-                }
-        }
-
-        if (sign == -1)
-                output = -result;
-        else
-                output = result;
-
-return (output);
->>>>>>> 0dc74d5db6e88cc9cc50667ddaa49744cb861f25
 }
+
