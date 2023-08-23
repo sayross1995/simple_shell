a34c1d40e2a6a06786095970fc838dfbd252a67b
anwar011 <elmouharibi12@gmail.com>
2023-08-23 07:01:10 -0700

    1

---
+++ simple_shell_task1.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+#define PROMPT "$ "
+
+
+int main(void)
+{
+	char *input = NULL; 
+	size_t n = 0;
+	char *token;
+	ssize_t nread;
+
+	while (1) {
+		write(STDOUT_FILENO, PROMPT, sizeof(PROMPT) - 1);
+
+		// Read input from user using getline
+		nread = getline(&input, &n, stdin);
+
+		if (nread == -1) {
+			perror("getline");
+			exit(EXIT_FAILURE);
+		} else if (nread == 1) {
+			// Handle end of file (Ctrl+D)
+			write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
+		// Remove the newline character at the end of the input
+		input[nread - 1] = '\0';
+
+		// Tokenize the input
+		token = strtok(input, " "); // Split by space
+
+		if (token != NULL) {
+			// Handle built-in command: exit
+			if (strcmp(token, "exit") == 0) {
+				free(input); // Free allocated memory before exit
+				exit(EXIT_SUCCESS);
+			}
+
+			// Fork and execute the command
+			pid_t pid = fork();
+
+			if (pid == -1) {
+				perror("fork");
+			} else if (pid == 0) {
+				// Child process
+				if (execve(token, &token, NULL) == -1) {
+					perror("execve");
+					exit(EXIT_FAILURE);
+				}
+			} else {
+				// Parent process
+				waitpid(pid, NULL, 0); // Wait for child to finish
+			}
+		}
+	}
+
+	free(input); // Free allocated memory before exiting
+	return 0;
+}
+
